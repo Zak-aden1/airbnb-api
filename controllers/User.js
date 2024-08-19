44932491ead501262c12b547cbb84b343d0f8223
zak aden <s.aden159@gmail.com>
2024-08-19 16:50:32 +0100

    setup user routes and model

--- controllers/User.js
+++ controllers/User.js
@@ -1,12 +1,20 @@
+const User = require('../models/Users')
 
-// login use
+// login user
 const loginUser = (req, res) => {
-
+  res.json('login route')
 }
 
 // signup user
-const signupUser = (req, res) => {
+const signupUser = async function(req, res) {
+  const {email, password, username} = req.body
 
+  try {
+    const user = await User.signup(email, password, username)
+    res.status(200).json({email, user })
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 module.exports = {
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,6 +17,21 @@ const userSchema = new mongoose.Schema({
   }
 })
 
-const UserModel = mongoose.model('user', userSchema)
+// static sign up method
+userSchema.statics.signup = async function (email, password, username) {
+  const exists = await this.findOne({ email })
+  if (exists) {
+    throw Error('email already exists')
+  }
+
+  const salt = await bcrypt.genSalt()
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({email, password: hash, username})
+
+  return user
+}
+
+const UserModel = mongoose.model('User', userSchema)
 
 module.exports = UserModel
